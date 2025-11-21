6969e4fd2c6b87df24feb785b861c11682aed28d
Geronimo Makowiecki <gmakowiecki@alu.frlp.utn.edu.ar>
2025-11-21 21:06:44 +0000

    chore: eliminar archivos obsoletos

--- src/routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleMessage } = require('../controllers/messageController');
-
-router.post('/', handleMessage);
-
-module.exports = router;
